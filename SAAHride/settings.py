99728247442e3c8a29d308fa8d4522168716bc92
francissaahtamba <francissaaht@gmail.com>
2025-07-10 05:15:37 +0000

    Trigger restart

--- SAAHride/settings.py
+++ SAAHride/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['saahride.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
